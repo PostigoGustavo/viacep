ef235ea5f04f3a0115d680ff8fcf560c6f97284d
PostigoGustavo <postigo.gustavo20@outlook.com>
2025-10-30 09:16:00 -0300

    Adicionando classe Endereco

---
+++ projeto-viacep/src/test/java/br/com/consulta_cep/ConsultaCepApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.consulta_cep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaCepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
